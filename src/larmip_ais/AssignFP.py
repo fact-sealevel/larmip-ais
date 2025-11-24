b8dd9f81adabf5cc2522cf4074baee46182d395e
Emma Marshall <emarshall@rhg.com>
2025-11-24 16:39:07 -0700

    formatting

--- src/larmip_ais/AssignFP.py
+++ src/larmip_ais/AssignFP.py
@@ -2,7 +2,7 @@ import numpy as np
 from larmip_ais.ReadFingerprint import ReadFingerprint as readfp
 from scipy import interpolate as interp
 
-''' AssignFP.py
+""" AssignFP.py
 
 Assigns interpolated fingerprint coefficients to sites identified by the vectors
 of lats and lons provided.
@@ -16,19 +16,22 @@ Return:
 fp_sites = Vector of fingerprint coefficients for the sites of interest
 
 
-'''
+"""
+
 
 def AssignFP(fp_filename, qlats, qlons):
-	## Read in the fitted parameters from parfile
-	# Open the file
-	try:
-		(fp, fp_lats, fp_lons) = readfp(fp_filename)
-	except:
-		print("Cannot open fingerprint file\n")
+    ## Read in the fitted parameters from parfile
+    # Open the file
+    try:
+        (fp, fp_lats, fp_lons) = readfp(fp_filename)
+    except ValueError:
+        print("Cannot open fingerprint file\n")
 
-	# Interpolate the fingerprint to these locations
-	lat_sort = np.argsort(fp_lats)
-	fp_interp = interp.RectBivariateSpline(fp_lats[lat_sort], fp_lons, fp[lat_sort,:], kx=1, ky=1)
-	fp_sites = fp_interp.ev(qlats, np.mod(qlons, 360))*1000
-	
-	return(fp_sites)
+    # Interpolate the fingerprint to these locations
+    lat_sort = np.argsort(fp_lats)
+    fp_interp = interp.RectBivariateSpline(
+        fp_lats[lat_sort], fp_lons, fp[lat_sort, :], kx=1, ky=1
+    )
+    fp_sites = fp_interp.ev(qlats, np.mod(qlons, 360)) * 1000
+
+    return fp_sites
--- src/larmip_ais/ReadFingerprint.py
+++ src/larmip_ais/ReadFingerprint.py
@@ -1,8 +1,6 @@
-import numpy as np
-import sys
 from netCDF4 import Dataset
 
-''' ReadFingerprint.py
+""" ReadFingerprint.py
 
 Provides a function that reads in a fingerprint data file from the netCDF files created
 from the original spherical harmonics files.
@@ -15,21 +13,20 @@ f = The fingerprint coefficient along a lat/lon grid [nlat x nlon]
 lat = Vector of latitudes
 lon = Vector of longitudes
 
-'''
+"""
+
 
 def ReadFingerprint(fname):
+    # Open the fingerprint file
+    try:
+        nc_fid = Dataset(fname, "r")
+    except:
+        print("Cannot open fingerprint file: {0}\n".format(fname))
+        raise
 
-	# Open the fingerprint file
-	try:
-		nc_fid = Dataset(fname, 'r')
-	except:
-		print("Cannot open fingerprint file: {0}\n".format(fname))
-		raise
-	
-	# Read in the fingerprint data
-	fp = nc_fid.variables['fp'][:,:]
-	fp_lats = nc_fid.variables['lat'][:]
-	fp_lons = nc_fid.variables['lon'][:]
+    # Read in the fingerprint data
+    fp = nc_fid.variables["fp"][:, :]
+    fp_lats = nc_fid.variables["lat"][:]
+    fp_lons = nc_fid.variables["lon"][:]
 
-	return(fp, fp_lats, fp_lons)
-	
+    return (fp, fp_lats, fp_lons)
